1b3014316bd91a7ea28cba7c25e293d9b2f9c458
Nicholas Keller <nic.keller328@gmail.com>
2020-04-04 17:53:19 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,70 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  attr_writer :name 
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_hash = {}
+  @@artist_hash = {}
+  
+  def initialize(name, artist, genre)
+  @@count += 1
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@genres << genre
+  @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    i = 0
+    while i < @@genres.length
+      if @@genre_hash.empty?
+        @@genre_hash[@@genres[i]] = [i]
+      elsif @@genre_hash.keys.include?(@@genres[i])
+        @@genre_hash[@@genres[i]] << name
+      else
+        @@genre_hash[@@genres[i]] = [i]
+      end
+      i += 1
+    end
+    count_hash = {}
+    @@genre_hash.each do |genre, number|
+      count_hash[genre] = number.count
+    end
+    count_hash
+  end
+  
+  def self.artist_count
+    i = 0
+    while i < @@artists.length
+      if @@artist_hash.empty?
+        @@artist_hash[@@artists[i]] = [i]
+      elsif @@artist_hash.keys.include?(@@artists[i])
+        @@artist_hash[@@artists[i]] << name
+      else
+        @@artist_hash[@@artists[i]] = [i]
+      end
+      i += 1
+    end
+    count_hash = {}
+    @@artist_hash.each do |artist, number|
+      count_hash[artist] = number.count
+    end
+    count_hash
+  end
+
+end
