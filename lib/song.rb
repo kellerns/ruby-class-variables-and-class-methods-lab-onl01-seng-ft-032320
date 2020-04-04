1d6583804c368276c3fc390377ed44d99dd8dd15
Nicholas Keller <nic.keller328@gmail.com>
2020-04-04 17:16:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,19 @@ class Song
   attr_accessor :name, :artists, :genres
   attr_writer :name 
   
-  @@song_count = 0
+  @@count = 0
   @@Genre_count = 0
   @@artist_count = 0
   
   def initialize
-  @@song_count += 1
- 
+  @@count += 1
+  end
   
+  def self.count
+    @@count
+  end
   
+  def self.genres
   
   
   
